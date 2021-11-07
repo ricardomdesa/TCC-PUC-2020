409179d4f5dd231b7693a7d2f9c95442e1c31eb3
Ricardo Sa <ricardo.sa@shopee.com>
2021-11-07 19:35:08 -0300

    updates tunning proj

--- sklearn_proj/tunning_gradient.py
+++ sklearn_proj/tunning_gradient.py
@@ -1,11 +1,8 @@
 from sklearn.ensemble import GradientBoostingClassifier
 
-from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
-from sklearn.metrics import make_scorer, precision_score
+from sklearn.model_selection import RandomizedSearchCV
 
-import numpy as np
-
-from base_func import split_dados, avaliar, read_df
+from base_func import avaliar, read_df
 
 
 def definir_modelos_ml() -> dict:
@@ -28,77 +25,45 @@ def rodar_imprimir_modelos_ml(modelos, X_treino, X_teste, y_treino, y_teste):
 
 def fit_tunning_rand_search_gradient(X_treino, y_treino):
     print("Tunning randomized search Gradient Boost")
-    # Number of trees in random forest
-    n_estimators = [800, 1500]
-    # Learning rate default = 0.1
-    learning_rate = [0.01, 0.025, 0.05, 0.1]
-    # Number of features to consider at every split
-    max_features = ['auto', 'sqrt']
-    # Maximum number of levels in tree
-    max_depth = [10, 5, 3]
-    # Minimum number of samples required to split a node
-    min_samples_split = [2, 5, 10]
-    # Minimum number of samples required at each leaf node
-    min_samples_leaf = [2, 4]
-    # Criterion
-    criterion = ["friedman_mse", "squared_error"]
 
     # Create the random grid
-    random_grid = {'n_estimators': n_estimators,
-                   'learning_rate': learning_rate,
-                   'max_features': max_features,
-                   # 'max_depth': max_depth,
-                   'min_samples_split': min_samples_split,
-                   # 'min_samples_leaf': min_samples_leaf,
-                   'criterion': criterion}
+    params = {'n_estimators': [800, 900, 1000],
+                   'learning_rate': [0.01, 0.015, 0.009],
+                   'max_features': ['auto', 'sqrt'],
+                   'min_samples_split': [2, 5, 10],
+                   # 'min_samples_leaf': [2, 5, 10],
+                   'criterion': ["friedman_mse", "squared_error"]}
 
-    print(random_grid)
+    print(params)
 
     rf_random = RandomizedSearchCV(estimator=GradientBoostingClassifier(),
-                                   param_distributions=random_grid,
-                                   n_iter=100,
+                                   param_distributions=params,
+                                   # n_iter=100,
                                    cv=3,
                                    verbose=2,
-                                   scoring="recall_macro",
+                                   # scoring="balanced_accuracy",
+                                   scoring="recall",
                                    random_state=42,
                                    n_jobs=-1)
 
     # best params {'n_estimators': 1411, 'min_samples_split': 2, 'min_samples_leaf': 2, 'max_features': 'auto', 'max_depth': 110, 'learning_rate': 0.025, 'criterion': 'squared_error'}
-    # {'n_estimators': 800, 'min_samples_split': 2, 'max_features': 'sqrt', 'learning_rate': 0.01,'criterion': 'squared_error'}
-    # precision 1 - 0.53 0.55 0.54 - score 0.5454
+    # {'n_estimators': 1000, 'min_samples_split': 10, 'max_features': 'auto', 'learning_rate': 0.01, 'criterion': 'squared_error'}
+    # precision 1 - 0.57 0.68 0.62 - score 0.5609
 
     # Fit the random search model
     return rf_random.fit(X_treino, y_treino)
 
 
-def fit_tunning_grid_gradient(X_treino, y_treino):
-    print("Tunning Gradient boost after random search cv")
-
-    grid = GridSearchCV(
-            estimator=GradientBoostingClassifier(),
-            param_grid={
-                'criterion': ["squared_error"],
-                'learning_rate': [0.02, 0.025, 0.03],
-                'max_depth': [90, 110, 120],
-                'max_features': ['auto'],
-                'min_samples_leaf': [2, 3],
-                'min_samples_split': [2, 3],
-                'n_estimators': [1344, 1411]
-            },
-            cv=3,
-            n_jobs=-1,
-            verbose=2
-    )
-
-    return grid.fit(X_treino, y_treino)
-
-
 def run_gradient_boost_tunning(df):
+
+    X = df.drop(["decisao"], axis=1)
+    y = df["decisao"]
+
     # split database train and test
-    X_treino, X_teste, y_treino, y_teste = split_dados(df)
+    from sklearn.model_selection import train_test_split
+    X_treino, X_teste, y_treino, y_teste = train_test_split(X, y, test_size=0.15, random_state=2)
 
     resultado_grid = fit_tunning_rand_search_gradient(X_treino, y_treino)
-    # resultado_grid = fit_tunning_grid_gradient(X_treino, y_treino)
 
     print("Ajuste Gradient feito")
 
@@ -106,6 +71,7 @@ def run_gradient_boost_tunning(df):
     print("Best score", resultado_grid.best_score_)
 
     modelo_tunado = resultado_grid.best_estimator_
+
     previsoes = modelo_tunado.predict(X_teste)
 
     avaliar(y_teste, previsoes, "RandomForest Tunado")
